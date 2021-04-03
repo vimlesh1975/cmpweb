495f0694cb1de20ba81377b8da3386c57a5ba037
vimlesh kumar <vimlesh1975@hotmail.com>
2021-04-03 10:59:13 +0530

    batch transcode

--- ffmpeg.js
+++ ffmpeg.js
@@ -5,24 +5,22 @@ var procffmpeg;
 
 var numvideo = 0;
 let videos = [];
+let transcodingparametrs;
 
 app.post('/transcode', (req, res) => {
 	res.end();
-	var aa = req.body.cmd;
-	
-	videos.unshift(aa);
+	videos.unshift(req.body.cmd);
 	numvideo += 1;
-	if (numvideo == 1) { processVideos(req.body.parameters) }
-	
+	transcodingparametrs = req.body['parameter1[]'];
+
+	if (numvideo == 1) { processVideos() }
 });
 
-function transcodeVideo(video, parameters) {
+function transcodeVideo(video) {
 	var filenamewithextension = video.split('\\').pop().split('/').pop();
 	var filename = path.parse(filenamewithextension).name;
 	var p = new Promise((resolve, reject) => {
-
 		procffmpeg = ffmpeg(video)
-
 			.on('end', function () {
 				resolve();
 				io.emit('transcodestatus', { data1: 'file has been converted succesfully' });
@@ -41,32 +39,33 @@ function transcodeVideo(video, parameters) {
 				}
 			})
 			.on('start', (data) => {
+				console.log(data);
 				io.emit('filenames', { originalfile1: video, transcodedfilename1: 'd:/test/' + filename + '_transcoded.mxf' });
 			})
 			.on('stderr', function (stderrLine) {
 				io.emit('transcodestatus', { data1: stderrLine });
 			})
 
-			.outputOptions(parameters)
+			.outputOptions(transcodingparametrs)
 			.save('d:/test/' + filename + '_transcoded.mxf');
 
 	});
 	return p;
 }
-function processVideos(parameters) {
-    let video = videos.pop();
-    if (video) {
-        transcodeVideo(video, parameters).then(() => {
-            console.log(`Completed Video - ${video}`);
-            processVideos();
-        }).catch(()=>{
+function processVideos() {
+
+	let video = videos.pop();
+	if (video) {
+		transcodeVideo(video).then(() => {
+			console.log(`Completed Video - ${video}`);
+			processVideos();
+		}).catch(() => {
 			console.log(`error in Transcoding Video - ${video}`);
-            processVideos();
+			processVideos();
 		});
-    }else
-    {
-        numvideo=0
-    }
+	} else {
+		numvideo = 0
+	}
 }
 
 var fluentffmpegutil = require('fluent-ffmpeg-util');
